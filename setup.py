5d8dbcfeb6ab3d6e3f61c3dd15847c395a5afdd1
Atze00 <lory.atze@gmail.com>
2021-07-12 14:18:15 +0200

    create a package

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='MoViNet-pytorch',
+    version='0.0.1',
+    author='Lorenzo Atzeni',
+    author_email='loryatze@gmail.com',
+    description='MoViNet pytorch implementation',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Atze00/MoViNet-pytorch',
+    license='MIT',
+    packages=find_packages(exclude=["tests"]),
+    install_requires=['torch',
+                      'fvcore',
+                     ],
+)
