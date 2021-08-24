bcb9788101a029b5052bb05782fc881a70a54bb6
Atze00 <lory.atze@gmail.com>
2021-08-24 10:54:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='MoViNet-pytorch',
-    version='0.2.0',
+    version='0.2.1',
     author='Lorenzo Atzeni',
     author_email='loryatze@gmail.com',
     description='MoViNet pytorch implementation',
